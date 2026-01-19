be6e156561879057c2740b4eb48e6d24ceb576a4
Shravani-Dake <shravanidake17@gmail.com>
2026-01-19 23:53:27 +0530

    Experiment-2 Uploaded

---
+++ Experiments/2/encapsulation.cs
@@ -0,0 +1,47 @@
+/* 
+Encapsulation : Encapsulation means wrapping data and methods into one unit and 
+                hiding data from outside access.
+                In short Encapsulation means hiding data and 
+                allowing access only through methods.
+                Data is private
+                Access is given using public methods
+*/
+
+using System;
+
+class Student
+{
+    // Private data members (hidden from outside)
+    private string name;
+    private int age;
+
+    // Public method to set values
+    public void SetDetails(string n, int a)
+    {
+        name = n;
+        age = a;
+    }
+
+    // Public method to get values
+    public void ShowDetails()
+    {
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student s1 = new Student();
+
+        // Setting values using public method
+        s1.SetDetails("Shravani", 20);
+
+        // Accessing values using public method
+        s1.ShowDetails();
+
+        Console.ReadLine();
+    }
+}
