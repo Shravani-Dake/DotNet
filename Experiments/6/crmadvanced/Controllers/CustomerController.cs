715dd461125b4c6103259ecd98d09b1dcd9a3d55
Shravani-Dake <shravanidake17@gmail.com>
2026-04-21 09:57:25 +0530

    Experiment-6 Uploaded

---
+++ Experiments/6/crmadvanced/Controllers/CustomerController.cs
@@ -0,0 +1,12 @@
+
+using Microsoft.AspNetCore.Mvc;
+// using crmadvanced.Models;
+// namespace crmadvanced.Controllers;
+public class CustomerController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
---
+++ Experiments/6/crmadvanced/Models/customer.cs
@@ -0,0 +1,7 @@
+public class customer
+{
+    public int id {get ; set ;}
+    public string name { get ; set ;}
+    public string email { get ; set ;}
+    public int age { get ; set ;}
+}
